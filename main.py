abfa7414075bff94b83f33e2ea9ff96e36e3dabe
REMKAD <106969156+REMKAD@users.noreply.github.com>
2022-07-09 16:22:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from cv2.cv2 import cv2
+import numpy as np
+
+
+def init_net():
+    input_nodes = 784
+    print('Введите число скрытых нейронов')
+    hidden_nodes = int(input())
+    out_nodes = 10
+    print('Введите скорость обучения')
+    learn_node = float(input())
+    return input_nodes, hidden_nodes, out_nodes, learn_node
+
+
+def create_net(input_nodes, hidden_nodes, out_nodes):
+    input_hidden_w = (np.random.rand(hidden_nodes, input_nodes) - 0.5)
+    hidden_out_w = (np.random.rand(out_nodes, hidden_nodes) - 0.5)
+    return input_hidden_w, hidden_out_w
+
+
